70497d2a9c70dab123bbb76c2d3c64fa17aa22d4
Maria Garriga <mariagarriga10@gmail.com>
2022-04-21 17:05:42 +0200

    *lineas descomentadas*
    fallo tonto🤡

--- src/gamejava/GameJava.java
+++ src/gamejava/GameJava.java
@@ -44,9 +44,9 @@ public class GameJava {
         /////////////////////////////////////////////////////
         //////////////   EMPIEZA EL PROGRAMA   //////////////
         /////////////////////////////////////////////////////
-        //Screens.printWaitScreen(); //pantalla de espera para empezar
-        //Screens.loadingAnimation(); //animacion de carga
-        //Screens.startMenu(menuOption); //pantalla menu
+        Screens.printWaitScreen(); //pantalla de espera para empezar
+        Screens.loadingAnimation(); //animacion de carga
+        Screens.startMenu(menuOption); //pantalla menu
 
         do {
             do {
