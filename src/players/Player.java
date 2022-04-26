9f3b87c66f52afa62d03d3476e91848f2a2fb02e
VictorGuill <victorguillofficial@gmail.com>
2022-04-26 21:48:30 +0200

    Añadido comentario al MOV

--- src/players/Player.java
+++ src/players/Player.java
@@ -19,11 +19,12 @@ public abstract class Player {
     }
 
     //Métodos
-    
-  
     /**
-     * Función que hace avanzar una posición en horizontal en caso de que la casilla siguiente sea nula o contenga una moneda(al posicionarse por encima se sumara 1 a las coins totales y se borrara)
-     *  @param mov
+     * Función que hace avanzar una posición en horizontal en caso de que la
+     * casilla siguiente sea nula o contenga una moneda(al posicionarse por
+     * encima se sumara 1 a las coins totales y se borrara)
+     *
+     * @param mov Cantidad de casillas a mover en la direccion.
      */
     public static void movXPositive(int mov) {
         String nextPosition = nextXPositive(mov);
@@ -31,39 +32,39 @@ public abstract class Player {
             En caso de que la siguiente casilla sea un vacio o una moneda entrara, después cojera la casilla donde te encuentres actualmente y la 
             establecera como un voidSquare , sumará 1 en la xpos y mandara a saveCharacter el string right que pondra la espada del jugador mirando hacia la derecha
             Finalmente guardara el personaje en la nueva posición
-        */
+         */
         if (nextPosition.equals(Board.voidSquare) || nextPosition.equals(Board.Coin)) {
             GameJava.board[Player.getYpos()][Player.getXpos()] = Board.voidSquare;
             Player.xpos += mov;
             direction = "right";
             Board.saveCharacter(direction);
             GameJava.board[Player.getYpos()][Player.getXpos()] = Board.Character;
-            
+
             //En caso de que la siguiente casilla sea una moneda sumara 1 al recuento de monedas y al colocarse encima borrara la coin del mapa.
-            
             if (nextPosition.equals(Board.Coin)) {
                 gainCoins();
             }
         }
-        
+
         // En caso de llegar a los limites derechos del mapa dara la vuelta a la posición de la espada utilizando la función saveCharacter del Board
-        
         if (Player.xpos == GameJava.widthBoard - 1) {
             direction = "left";
             Board.saveCharacter(direction);
             GameJava.board[Player.getYpos()][Player.getXpos()] = Board.Character;
         }
     }
-    /**
-     * Función que hace retroceder una posición en horizontal en caso de que la casilla siguiente sea nula o contenga una moneda(al posicionarse por encima se sumara 1 a las coins totales y se borrara)
-     *  @param mov
-     */
 
+    /**
+     * Función que hace retroceder una posición en horizontal en caso de que la
+     * casilla siguiente sea nula o contenga una moneda(al posicionarse por
+     * encima se sumara 1 a las coins totales y se borrara)
+     *
+     * @param mov Cantidad de casillas a mover en la direccion.
+     */
     public static void movXNegative(int mov) {
         String nextPosition = nextXNegative(mov);
-        
+
         // Funcionamiento similar a movXPositive solo que está vez hacia el lado izquiero del tablero, saveCharacter pondrá la espada mirando hacia la izquierda
-        
         if (nextPosition.equals(Board.voidSquare) || nextPosition.equals(Board.Coin)) {
             GameJava.board[Player.getYpos()][Player.getXpos()] = Board.voidSquare;
             Player.xpos -= mov;
@@ -75,23 +76,26 @@ public abstract class Player {
             }
         }
         //En caso de llegar a los limites izquierdos del tablero cambiara la dirección de la espada hacia la derecha
-        
+
         if (Player.xpos == 0) {
             direction = "right";
             Board.saveCharacter(direction);
             GameJava.board[Player.getYpos()][Player.getXpos()] = Board.Character;
         }
     }
+
     /**
-     * Función que hace avanzar una posición en vertical en caso de que la casilla siguiente sea nula o contenga una moneda(al posicionarse por encima se sumara 1 a las coins totales y se borrara)
-     *  @param mov
+     * Función que hace avanzar una posición en vertical en caso de que la
+     * casilla siguiente sea nula o contenga una moneda(al posicionarse por
+     * encima se sumara 1 a las coins totales y se borrara)
+     *
+     * @param mov Cantidad de casillas a mover en la direccion.
      */
     public static void movYPositive(int mov) {
         String nextPosition = nextYPositive(mov);
-        
+
         //Funcionamiento similar a las dos funciones anteriores solo que esta vez en el eje de la y positivas (como es un array al subir una fila su numero desciende)
         //En este caso la dirección de la espada se queda igual que en el ultimo movimiento en horizontal
-        
         if (nextPosition.equals(Board.voidSquare) || nextPosition.equals(Board.Coin)) {
             GameJava.board[Player.getYpos()][Player.getXpos()] = Board.voidSquare;
             Player.ypos -= mov;
@@ -102,16 +106,18 @@ public abstract class Player {
             }
         }
     }
-    /**
-     * Función que hace retroceder una posición en vertical en caso de que la casilla siguiente sea nula o contenga una moneda(al posicionarse por encima se sumara 1 a las coins totales y se borrara)
-     *  @param mov
-     */
 
+    /**
+     * Función que hace retroceder una posición en vertical en caso de que la
+     * casilla siguiente sea nula o contenga una moneda(al posicionarse por
+     * encima se sumara 1 a las coins totales y se borrara)
+     *
+     * @param mov Cantidad de casillas a mover en la direccion.
+     */
     public static void movYNegative(int mov) {
         String nextPosition = nextYNegative(mov);
-        
+
         //Funcionamiento similar a las dos funciones anteriores solo que esta vez en el eje de la y negativas (como es un array al bajar una fila su numero asciende)
-        
         if (nextPosition.equals(Board.voidSquare) || nextPosition.equals(Board.Coin)) {
             GameJava.board[Player.getYpos()][Player.getXpos()] = Board.voidSquare;
             Player.ypos += mov;
@@ -124,8 +130,10 @@ public abstract class Player {
     }
 
     /**
-     * Función que te retorna que hay en la siguiente posición en horizontal del array basandose en la xpos y ypos del Player
-     * @param mov
+     * Función que te retorna que hay en la siguiente posición en horizontal del
+     * array basandose en la xpos y ypos del Player
+     *
+     * @param mov Cantidad de casillas a mover en la direccion.
      * @return nextPostition
      */
     public static String nextXPositive(int mov) {
@@ -137,12 +145,14 @@ public abstract class Player {
         }
         return nextPosition;
     }
-     /**
-     * Función que te retorna que hay en la anterior posición en horizontal del array basandose en la xpos y ypos del Player
-     * @param mov
+
+    /**
+     * Función que te retorna que hay en la anterior posición en horizontal del
+     * array basandose en la xpos y ypos del Player
+     *
+     * @param mov Cantidad de casillas a mover en la direccion.
      * @return nextPostition
      */
-
     public static String nextXNegative(int mov) {
         String nextPosition = "DON'T EXISTS";
         int xTemp = Player.getXpos() - mov;
@@ -152,12 +162,14 @@ public abstract class Player {
         }
         return nextPosition;
     }
-      /**
-     * Función que te retorna que hay en la anterior posición en vertical del array basandose en la xpos y ypos del Player
-     * @param mov
+
+    /**
+     * Función que te retorna que hay en la anterior posición en vertical del
+     * array basandose en la xpos y ypos del Player
+     *
+     * @param mov Cantidad de casillas a mover en la direccion.
      * @return nextPostition
      */
-
     public static String nextYNegative(int mov) {
         String nextPosition = "DON'T EXISTS";
         int yTemp = Player.getYpos() + mov;
@@ -167,10 +179,12 @@ public abstract class Player {
         }
         return nextPosition;
     }
-    
-     /**
-     * Función que te retorna que hay en la siguiente posición en vertical del array basandose en la xpos y ypos del Player
-     * @param mov
+
+    /**
+     * Función que te retorna que hay en la siguiente posición en vertical del
+     * array basandose en la xpos y ypos del Player
+     *
+     * @param mov Cantidad de casillas a mover en la direccion.
      * @return nextPostition
      */
     public static String nextYPositive(int mov) {
@@ -183,9 +197,10 @@ public abstract class Player {
         return nextPosition;
     }
 
-    
     /**
-     * Compara la siguiente casilla en relación a la xPos y yPos del Player, en caso de que haya una moneda la borra del array y suma 1 al recuento de monedas del jugador
+     * Compara la siguiente casilla en relación a la xPos y yPos del Player, en
+     * caso de que haya una moneda la borra del array y suma 1 al recuento de
+     * monedas del jugador
      */
     public static void pickUpCoin() {
         boolean isCoin = false;
@@ -222,10 +237,11 @@ public abstract class Player {
 
     }
 
-   /**
-    * Método que inicia un ataque en caso de que la siguiente casilla en relación con el personaje sea uno de los posibles enemigos (estado normal 
-    * o cuando se encuentra mirando en alguna de las posiciones posibles
-    */
+    /**
+     * Método que inicia un ataque en caso de que la siguiente casilla en
+     * relación con el personaje sea uno de los posibles enemigos (estado normal
+     * o cuando se encuentra mirando en alguna de las posiciones posibles
+     */
     public static void basicAttack() {
         boolean isAttack;
 
