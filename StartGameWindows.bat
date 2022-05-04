f9a96fff149cc70d6eb3e251c06dc0ba09b9cbe5
VictorGuill <victorguillofficial@gmail.com>
2022-05-04 22:57:42 +0200

    BATCH SCRIPT WINDOWS UTF-8
    Script de batch para ejecutar el juego en CMD con la codificacion UTF-8.
    
    Al cerrar el juego, se quita el UTF-8 y se reestablece la opcion original del usuario en el CMD.

---
+++ StartGameWindows.bat
@@ -0,0 +1,26 @@
+::=====================================================================
+:: This game uses UTF-8 characters not available in CMD by default.
+::
+:: To guarantee UTF-8 is used in the CMD, this script changes your 
+:: current code page before playing and restores it back on exit.
+::
+::
+:: If you want to see your current code page, use 'chcp' in a CMD.
+:: More information about windows code pages below:
+:: https://docs.microsoft.com/en-us/windows/win32/intl/code-page-identifiers
+::=====================================================================
+
+
+@echo off
+
+::Gets 'chcp' output and stores the number in %_cp_%
+for /f "tokens=2 delims=:" %%G in ('chcp') do Set _cp_=%%G
+
+::Change CMD code page to UTF-8
+chcp 65001
+
+::Run jar file with UTF-8
+java -Dfile.encoding=UTF-8 -jar "./dist/GameJava.jar"
+
+::Restore original user code page on CMD
+chcp %_cp_%
